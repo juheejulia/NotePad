d244971d28e8023cdfe1bc0ebb98002e6646cabe
Juhee Kang <juhee.kang@infotiv.se>
2023-10-17 10:19:59 +0200

    create folders that needs

--- app/src/main/java/com/example/notepad/EditorActivity.java
+++ app/src/main/java/com/example/notepad/EditorActivity.java
@@ -4,12 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-// main page, It shows all notes lists and it is selectable
-public class MainActivity extends AppCompatActivity {
+public class EditorActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_editor);
     }
 }
